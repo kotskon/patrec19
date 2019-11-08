ee2b3e4304a5ac5c32c9db6f8e588c124001996f
Christos Lamprakos <cplamprakos@microlab.ntua.gr>
2019-11-08 21:41:05 +0200

    Prob'ly wrong move...

--- README.md
+++
@@ -1,2 +0,0 @@
-# patrec19
-Here will be hosted my solutions for the lab exercises of NTUA ECE's course of Pattern Recognition, winter semester of 2019-2020. 
--- labOne/config/__init__.py
+++
@@ -1,5 +0,0 @@
-class ToolConfig ():
-    def __init__ (self):
-        self.train = './data/train.txt'
-        self.test = './data/test.txt'
-        self.save = './results/'
