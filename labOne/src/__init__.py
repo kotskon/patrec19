a559b157291a89df28018b2aac698138c2b3053d
Christos Lamprakos <cplamprakos@microlab.ntua.gr>
2019-11-09 21:50:20 +0200

    Steps 1-11 completed. Slow code.

--- labOne/src/__init__.py
+++ labOne/src/__init__.py
@@ -1,5 +1,3 @@
-#Re-usability of the tool is a desired feature. Thus a (possibly
-#redundantly complex) module-based scheme will be employed.
 import config
 from src import datautilus
 import matplotlib.pyplot as plt
@@ -15,73 +13,70 @@ def main ():
     print ('Module initiated. Reading data...')
 
     #Step 1
-    [X_train, X_test, y_train, y_test] = datautilus.dataReader (
+    X_train, X_test, y_train, y_test = datautilus.dataReader (
                                          dataHouse.train,
                                          dataHouse.test)
     print ('All aboard!')
 
     #Step 2
     print ('Picking pattern #131...')
-    [feat_131, id_131] = datautilus.pickDigit (X_train, y_train, 131)
+    feat_131, id_131 = datautilus.pickDigit (X_train, y_train, 131)
     print ('Done! Number is ', int (id_131))
-    [step2_fig, axs] = plt.subplots ()
-    [axs, im] = datautilus.digit2Fig (feat_131, axs)
-    step2_fig.colorbar (im)
-    axs.set_ylabel ('pixel no.')
-    axs.set_xlabel ('pixel no.')
-    step2_fig.savefig(dataHouse.save + 'step2.svg')
-    #plt.show ()
-    plt.close ()
+    datautilus.printSingleDigit (feat_131, dataHouse.save, 'step2')
 
-    #Step 3
     print ('Commencing serial digit search...')
-    [posx, posy] = [0, 0]
-    [step3_fig, axs] = plt.subplots (2, 5, figsize = (20, 20))
+    posx, posy = 0, 0
+    step3_fig, axs3 = plt.subplots (2, 5, figsize = (20, 20))
+    step9_fig, axs9 = plt.subplots (2, 5, figsize = (20, 20))
+
+    #Step 10
+    means_overall = pd.DataFrame ()
+    vars_overall = pd.DataFrame ()
     for num in range(10):
+        #Step 3
         step3_num = datautilus.findDigit (X_train, y_train, num).iloc[0, :]
-        [axs[posx, posy], im] = datautilus.digit2Fig (step3_num,
-                                                      axs[posx, posy])
+        #Steps 6-9
+        mean_num, var_num = datautilus.analyzeDigit (X_train, y_train, num,
+                                                     dataHouse.save)
+        means_overall = means_overall.append (mean_num, ignore_index = True)
+        vars_overall = vars_overall.append (var_num, ignore_index = True)
+        axs3[posx, posy], im3 = datautilus.digit2Fig (step3_num,
+                                                      axs3[posx, posy])
+        axs9[posx, posy], im9 = datautilus.digit2Fig (mean_num,
+                                                      axs9[posx, posy])
+        step9_fig.colorbar (im9, ax = axs9[posx, posy], fraction = .04)
         posy += 1
         if posy % 5 == 0:
             posy = 0
             posx += 1
 
-    step3_fig.colorbar (im, ax = axs, orientation = 'horizontal',
+    step3_fig.colorbar (im3, ax = axs3, orientation = 'horizontal',
                         fraction = .025)
     step3_fig.savefig(dataHouse.save + 'step3.svg')
+    plt.close ()
+    step9_fig.tight_layout ()
+    step9_fig.savefig(dataHouse.save + 'step9.svg')
     #plt.show ()
     plt.close ()
 
-    #Step 4
-    print ('Collecting all instances of the digit 0...')
-    step4_feats = datautilus.findDigit (X_train, y_train, 0)
-    pix_1010_0 = datautilus.pixelSummoner (step4_feats, (10, 10))
-    print ('Mean value for 0 pixel (10, 10) is: ', pix_1010_0.mean ())
+    #Steps 4-5
+    flat_idx = 9 * 16 + 9
+    print ('For digit 0, mean value of pixel (10, 10) is ',
+           means_overall.iloc[0, :].values[flat_idx])
+    print ('For digit 0, variance of pixel (10, 10) is ',
+           vars_overall.iloc[0, :].values[flat_idx])
 
-    #Step 5
-    print ('Variance for 0 pixel (10, 10) is: ', pix_1010_0.var ())
+    #Step 10
+    feat_101, id_101 = datautilus.pickDigit (X_train, y_train, 101)
+    pred_101 = datautilus.askEuclid (means_overall, feat_101)
+    print ('prediction: ', pred_101)
+    print ('actual: ', int (id_101))
+    datautilus.printSingleDigit (feat_101, dataHouse.save, 'step10')
 
-    #Steps 6 - 8
-    mean_train_0 = []
-    var_train_0 = []
-    for i in range (16):
-        i += 1
-        for j in range (16):
-            j += 1
-            next_pix = datautilus.pixelSummoner (step4_feats, (i, j))
-            mean_train_0.append (next_pix.mean ())
-            var_train_0.append (next_pix.var ())
-    print ('Mean, var data for all pixels of 0 collected! Saving results...')
-    [steps6_8_fig, axs] = plt.subplots (1, 2, squeeze = False, figsize = (20, 20))
-    [axs[0, 0], im] = datautilus.digit2Fig (pd.Series (mean_train_0),
-                                                       axs[0, 0])
-    steps6_8_fig.colorbar (im, ax = axs[0, 0], fraction = .04)
-    [axs[0, 1], im] = datautilus.digit2Fig (pd.Series (var_train_0),
-                                                       axs[0, 1])
-    steps6_8_fig.colorbar (im, ax = axs[0, 1], fraction = .04)
-    steps6_8_fig.savefig(dataHouse.save + 'steps6_8.svg')
-    #plt.show ()
-    plt.close ()
+    #Step 11
+    score, pairs = datautilus.batchEuclid (X_test, y_test, means_overall)
+    print ('The trained Euclidean classifier performed ', score,
+           'successfully on the test dataset.')
 
 if __name__ != '__main__':
     main ()
--- labOne/src/datautilus.py
+++ labOne/src/datautilus.py
@@ -11,25 +11,62 @@ def dataReader (train_path, test_path):
     X_train = train_concat.iloc[:, 1:]
     y_test = test_concat.iloc[:, 0]
     X_test = test_concat.iloc[:, 1:]
-    return [X_train, X_test, y_train, y_test]
+    return X_train, X_test, y_train, y_test
 
 def digit2Fig(feature, ax):
     transformed = np.reshape (feature.values, (16, 16))
     im = ax.imshow (transformed, cmap = 'gist_yarg')
-    return [ax, im]
+    return ax, im
 
 def pickDigit (features, patterns, index):
     true_idx = index-1
     ind_feats = features.iloc[true_idx, :]
     ind_id = patterns.iloc[true_idx]
-    return [ind_feats, ind_id]
+    return ind_feats, ind_id
 
 def findDigit (features, patterns, value):
     indexes = patterns[patterns == float (value)]
     return features.iloc[indexes.index[:], :]
 
-def pixelSummoner (digit_feats, loc):
-    x = loc[0] - 1
-    y = loc[1] - 1
-    flat_pos = 16 * x + y
-    return digit_feats.iloc[:, flat_pos]
+def analyzeDigit (features, patterns, value, path):
+    feats_val = findDigit (features, patterns, value)
+    mean_val = feats_val.mean (axis = 0)
+    var_val = feats_val.var (axis = 0)
+    [steps6_8_fig, axs] = plt.subplots (1, 2, squeeze = False,
+                                        figsize = (20, 20))
+    [axs[0, 0], im] = digit2Fig (mean_val, axs[0, 0])
+    steps6_8_fig.colorbar (im, ax = axs[0, 0], fraction = .04)
+    [axs[0, 1], im] = digit2Fig (var_val, axs[0, 1])
+    steps6_8_fig.colorbar (im, ax = axs[0, 1], fraction = .04)
+    steps6_8_fig.savefig(path + str (value) + '_stats.svg')
+    plt.close ()
+    return mean_val, var_val
+
+def askEuclid (knowledge, feature):
+    dists = np.zeros (shape = (10, 1))
+    for i in range (knowledge.shape[0]):
+        dists[i] = np.linalg.norm (knowledge.iloc[i, :].values -
+                   feature.values)
+        #print ('Distance from ', i, ': ', dists[i])
+    return np.array (dists).argmin ()
+
+def printSingleDigit (feature, path, name):
+    fig, axs = plt.subplots ()
+    axs, im = digit2Fig (feature, axs)
+    fig.colorbar (im)
+    axs.set_ylabel ('pixel no.')
+    axs.set_xlabel ('pixel no.')
+    fig.savefig(path + name + '.svg')
+    plt.close ()
+
+def batchEuclid (features, patterns, knowledge):
+    featnum = features.shape[0]
+    correct = 0
+    pairs = pd.DataFrame ()
+    for i in range (featnum):
+        pred = askEuclid (knowledge, features.iloc[i, :])
+        pairs = pairs.append (pd.Series([int (patterns[i]), pred]),
+                              ignore_index = True)
+        if int (patterns[i]) == pred:
+            correct += 1
+    return correct / featnum * 100, pairs
