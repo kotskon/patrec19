758224a53fc2ca5a0f5125444bb18b9711bcfcf6
Christos Lamprakos <cplamprakos@microlab.ntua.gr>
2019-12-06 22:23:51 +0200

    Lab 2 commenced. First step complete.

---
+++ labTwo/config/__init__.py
@@ -0,0 +1,5 @@
+class ToolConfig ():
+    def __init__ (self):
+        self.one = './data/onetwothree1.wav'
+        self.two = './data/onetwothree8.wav'
+        self.digits_path = './data/digits'
