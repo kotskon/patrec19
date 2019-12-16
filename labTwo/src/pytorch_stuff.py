17d75138c545d637473d63eaa7ee8099fae9483e
Christos Lamprakos <cplamprakos@microlab.ntua.gr>
2019-12-16 18:34:57 +0200

    Step 10 in progress.

--- labTwo/src/pytorch_stuff.py
+++ labTwo/src/pytorch_stuff.py
@@ -15,35 +15,37 @@ class DigitsDataset (Dataset):
         return self.dad.shape[0]
 
     def __getitem__ (self, idx):
-        return self.data[idx, 1:], self.data[idx, 0]
 
-class Net0Hidden (nn.Module):
-    def __init__ (self):
-        super (Net0Hidden, self).__init__ ()
-        self.lin = nn.Linear (256, 10)
+        return self.data.view (self.dad.shape[0], 1, -1)[idx][0][:10],        \
+               self.data.view (self.dad.shape[0], 1, -1)[idx][0][10:]
 
-    def forward (self, batch):
-        #The activation function (log softmax) is encoded in the loss function
-        #used for fitting (cross_entropy)
-        return self.lin (batch)
+class DummyRNN (torch.nn.Module):
+    """
+    Implements a basic RNN architecture. Can process data in batches, as well
+    as individually.
+    """
+    def __init__ (self, input_size=1, hidden_layer_size=10, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.rnn = torch.nn.RNN (input_size, hidden_layer_size)
+        self.linear = torch.nn.Linear (hidden_layer_size, output_size)
+        self.hidden_cell = torch.zeros (1, 1, self.hidden_layer_size)
 
-class Net1Hidden (nn.Module):
-    #A single hidden layer is introduced. Its size can be set upon initializing.
-    def __init__ (self, hidden_size):
-        super (Net1Hidden, self).__init__ ()
-        self.lin1 = nn.Linear (256, hidden_size)
-        #The output is kept at the same size of 10 (so that log softmax still
-        #has meaning).
-        self.lin2 = nn.Linear (hidden_size, 10)
-
-    def forward (self, batch):
-        #ReLU is used as activation function for the input layer.
-        mid = F.relu (self.lin1 (batch))
-        return self.lin2 (mid)
+    def forward (self, input_seq):
+        seqNum = input_seq.view (-1, 1, 10).shape[0]
+        seqLength = input_seq.view (-1, 1, 10).shape[-1]
+        pred = torch.zeros (seqNum, 1, seqLength)
+        for s in range (seqNum):
+            for i in range (seqLength):
+                rnn_out, self.hidden_cell = self.rnn (input_seq.view        \
+                                            (seqNum, 1, seqLength)[s][0][i] \
+                                            .view (1, 1, -1), self.hidden_cell)
+                pred[s][0][i] = self.linear (self.hidden_cell)
+        return pred
 
 def fit (net, epochs, lr, loader, v_loader):
     #Define loss function.
-    loss_func = F.cross_entropy
+    loss_func = F.mse_loss
     #Use Stochastic Gradient Descent of the optim package for parameter updates.
     opt = optim.SGD (net.parameters (), lr = lr)
     #A flag  for validation loss.
@@ -54,13 +56,15 @@ def fit (net, epochs, lr, loader, v_loader):
         net.train ()
         for xb, yb in loader:
             out = net.forward (xb)
-            loss = loss_func (out, yb.type ('torch.LongTensor'))
+            loss = loss_func (out, yb.view (-1, 1, 10))
             #Backpropagate the loss.
             loss.backward ()
             #Update weights, bias.
             opt.step ()
             #A new batch is ready to be loaded. Clean gradient memory!
             opt.zero_grad ()
+            #Reset hidden gradient between batches.
+            net.hidden_cell.detach_ ()
         #At the end of each epoch, the network is put in evaluation mode.
         net.eval ()
         #No reason to keep the gradient for the validation set.
@@ -68,25 +72,7 @@ def fit (net, epochs, lr, loader, v_loader):
             valid_loss = 0
             for xb, yb in v_loader:
                 valid_loss += loss_func (net.forward (xb), \
-                                         yb.type ('torch.LongTensor'))
-            #Early stopping!
-            if old_valid_loss != -62 and valid_loss > old_valid_loss:
-                break
-            old_valid_loss = valid_loss
-    print ('Training finished due to early stopping. Actual number of epochs: ',
-           epoch)
-    return net
-
-def netScore (net, X):
-    features = X[:][0]
-    patterns = X[:][1]
-    preds = net.forward (features).argmax (axis = 1).type ('torch.FloatTensor')
-    patterns = patterns.reshape (1, -1).numpy ()
-    preds = preds.reshape (1, -1).numpy ()
-    return (patterns == preds).sum () / preds.size
-
-def training_wrapper (net, epochs, lr, loader, v_loader, test_tensor):
-    print ('Score before training: ', netScore (net, test_tensor))
-    net = fit (net, epochs, lr, loader, v_loader)
-    print ('Score after training: ', netScore (net, test_tensor))
+                                         yb.view (-1, 1, 10))
+            print ('Epoch', epoch, 'finished with val loss:', valid_loss)
+    print ('Training finished.')
     return net
